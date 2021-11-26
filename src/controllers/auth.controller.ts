d8de44f67ef98ce233d2057e6c2cb179c9dbc2b0
Desire Kaleba <desirekaleba@gmail.com>
2021-11-26 20:41:30 +0300

    feat: add user signin

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from 'express';
 import { AuthService } from '../database/services/auth.service';
 
-import { hash as hashPassword } from '../utils/password';
+import { hash as hashPassword, compare as comparePassword } from '../utils/password';
 import { generate as generateToken } from '../utils/token';
-import { success } from '../utils/response';
-import { created } from '../constants/responseMessages';
+import { error, success } from '../utils/response';
+import { created, notFound } from '../constants/responseMessages';
 import { CreateUserDTO } from '../dtos/createUser.dto';
-import { CREATED } from '../constants/statusCodes';
+import { CREATED, NOT_FOUND, OK, UNAUTHORIZED } from '../constants/statusCodes';
+import { SigninUserDTO } from '../dtos/signinUser.dto';
 
 /**
  * Auth Controller
@@ -40,6 +41,46 @@ export class AuthController {
         token,
         email: user.email,
       },
+      res,
+    });
+  };
+
+  /**
+   * signin
+   * @param { req: Request }
+   * @param { res: Response }
+   * @returns { Object: User }
+   * @memberOf AuthController
+   */
+  signin = async (req: Request, res: Response): Promise<Response> => {
+    const signinData: SigninUserDTO = req.body;
+
+    const user = await this.authService.signin({ ...signinData });
+
+    if (user) {
+      if (comparePassword(signinData.password, user.password)) {
+        const token = generateToken(user.id);
+        return success({
+          code: OK,
+          message: 'user found.',
+          data: {
+            token,
+            userId: user.id,
+            email: user.email,
+            isAdmin: user.isAdmin,
+          },
+          res,
+        });
+      }
+      return error({
+        code: UNAUTHORIZED,
+        message: `Incorrect password.`,
+        res,
+      });
+    }
+    return error({
+      code: NOT_FOUND,
+      message: notFound(`User with '${signinData.email}'`),
       res,
     });
   };
---
+++ src/dtos/signinUser.dto.ts
@@ -0,0 +1,4 @@
+export interface SigninUserDTO {
+  email: string;
+  password: string;
+}
